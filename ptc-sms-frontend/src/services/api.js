7db86db92aea24db8e6d53908b2810348ce93bda
reicabenito <reicabenito26@gmail.com>
2025-12-14 17:41:40 +0800

    Update API baseURL to live Render backend

--- ptc-sms-frontend/src/services/api.js
+++ ptc-sms-frontend/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api', 
+    baseURL: 'https://final-project-sms-bgd4.onrender.com/api', 
     headers: {
         'Content-Type': 'application/json'},
 });
